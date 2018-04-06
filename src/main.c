30aa51116fff4d001052813fe64c7fdd4203550c
antonioseis <tonilopes96@live.com.pt>
2018-04-06 18:41:56 +0100

    pacificio, isto já compila e corre. experimenta!

--- src/main.c
+++ src/main.c
@@ -1,8 +1,42 @@
 #include <date.h>
 #include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 
-int main(){
-  Date d = createDate(1,2,2017);
-  printf("Dia: %i\n", get_day(d));
-  return 0;
-} 
+static void print_element_names(xmlNode * a_node){
+    xmlNode *cur_node = NULL;
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+         if (cur_node->type == XML_ELEMENT_NODE) {
+             printf("%s:\n", cur_node->name);
+             xmlAttr* attribute = cur_node->properties;
+             while (attribute && attribute->name && attribute->children) {
+                 xmlChar* value = xmlNodeListGetString(cur_node->doc, attribute->children, 1);
+                 printf("\t%s: %s\n", attribute->name, value);
+                 xmlFree(value);
+                 attribute = attribute->next;
+             }
+             printf("----------------------------------------\n");
+         }
+        print_element_names(cur_node->children);
+   }
+}
+
+int main(int argc, char **argv){
+   xmlDoc *doc = NULL;
+   xmlNode *root_element = NULL;
+  
+   if (argc != 2)  return(1);
+   LIBXML_TEST_VERSION    // Macro to check API for match with
+                          // the DLL we are using
+    //parse the file and get the DOM
+   if ((doc = xmlReadFile(argv[1], NULL, 0)) == NULL){
+       printf("error: could not parse file %s\n", argv[1]);
+       exit(-1);
+   }
+    //Get the root element node 
+    root_element = xmlDocGetRootElement(doc);
+    print_element_names(root_element);
+    xmlFreeDoc(doc);       // free document
+    xmlCleanupParser();    // Free globals
+    return 0;
+}
