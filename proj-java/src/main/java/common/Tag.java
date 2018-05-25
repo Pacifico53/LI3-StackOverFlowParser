ebd59a6a33e9574655285546d9781b99d8cc88ba
videira59 <nandofmap@gmail.com>
2018-05-25 22:49:37 +0100

    tag criada

---
+++ proj-java/src/main/java/common/Tag.java
@@ -0,0 +1,62 @@
+package common;
+
+import java.util.Objects;
+
+public class Tag {
+    private String tagname;
+    private int count;
+    private long tag_id;
+
+    public Tag(String tagname, int count, long tag_id) {
+        this.tagname = tagname;
+        this.count = count;
+        this.tag_id = tag_id;
+
+    }
+
+    public Tag(){ this("", 0, 0);}
+
+    public Tag (Tag t){
+        this.tagname = t.getTagname();
+        this.count = t.getCount();
+        this.tag_id = t.getTag_id();
+    }
+
+    public String getTagname() {
+        return tagname;
+    }
+
+    public void setTagname(String tagname) {
+        this.tagname = tagname;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public long getTag_id() {
+        return tag_id;
+    }
+
+    public void setTag_id(long tag_id) {
+        this.tag_id = tag_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tags = (Tag) o;
+        return count == tags.count &&
+                tag_id == tags.tag_id &&
+                Objects.equals(tagname, tags.tagname);
+    }
+
+    public Tag clone (){
+        return new Tag(this);
+    }
+}
