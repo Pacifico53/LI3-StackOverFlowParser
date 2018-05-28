a57f2dd7968be8d39158d42bc1660b8ca3e7ce10
antonioseis <tonilopes96@live.com.pt>
2018-05-28 22:32:23 +0100

    Iniciando o init calendario

---
+++ proj-java/src/main/java/common/DataCalendar.java
@@ -0,0 +1,43 @@
+package common;
+
+import java.util.ArrayList;
+
+public class DataCalendar {
+    private ArrayList<Year> years;
+
+    public DataCalendar(ArrayList<Year> years) {
+        this.years = years;
+    }
+
+    public DataCalendar(DataCalendar d){
+        this.years = d.getYears();
+    }
+
+    public DataCalendar(){
+        this.years = new ArrayList<Year>(9);
+    }
+
+    public ArrayList<Year> getYears() {
+        return years;
+    }
+
+    public void setYears(ArrayList<Year> years) {
+        this.years = years;
+    }
+
+
+    public void init(){
+        for(Year y: this.years){
+            ArrayList<Month> months = new ArrayList<Month>(12);
+            for (Month m: months){
+                ArrayList<Day> days = new ArrayList<Day>(31);
+                for(Day d: days) {
+                    ArrayList<Long> ids = new ArrayList<Long>();
+                    d.getIds().add(ids);
+                }
+                m.getDays().add(days);
+            }
+            y.getMonths().add(months);
+        }
+    }
+}
