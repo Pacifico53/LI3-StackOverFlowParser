907fa8d910112cc7b6fb740f2736929a6ec71c6d
Pacifico53 <bernardo2viseu@gmail.com>
2018-05-28 16:11:12 +0100

    aceita input do path

--- proj-java/src/main/java/li3/Main.java
+++ proj-java/src/main/java/li3/Main.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
 
-        String path = "../../../li3/android";
+        String path = args[0];
         long before, after;
 
         System.out.println("Path = \"" + path + "\"");
